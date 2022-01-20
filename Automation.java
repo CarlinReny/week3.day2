459aaf3237a164de40e70b64ce37b907d4588a73
CarlinReny <96654645+CarlinReny@users.noreply.github.com>
2022-01-20 12:59:06 +0530

    Assignment 1

---
+++ Automation.java
@@ -0,0 +1,30 @@
+package week3.day2;
+
+public abstract class Automation extends MultipleLanguage implements Testtool, Language{
+
+	public void selenium() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void ruby() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	}
+
+		
+	
+	
+	
+	
+
+
---
+++ MultipleLanguage.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+public abstract class MultipleLanguage{
+	
+	void python(){
+		
+		
+		System.out.println("This is python-implemented method");
+	}
+	
+ abstract void ruby();
+	
+	
+
+}
